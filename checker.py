dcfe85dc1183e369ccad89cccf5171e9d55dcd64
dadayasho <lmaolol228@mail.ru>
2023-10-23 22:43:27 +0300

    [code added\]

--- checker.py
+++ checker.py
@@ -1,13 +1,15 @@
 import re
-import csv
-import docx
+
 
 class Checker:
     def __init__(self, file_path):
         self.file = file_path
 
     def check_file(self):
-        with open(self.file, 'r', endcoding='utf-8') as file_txt:
+        with open(self.file, 'r') as file_txt:
             content = file_txt.read()
+            content = re.sub(r'\n', ' ', content)
+            pattern = (r'\b[23561]\d{12}(?:\d{3}|\d{5}|\d{7}|\d{8})?\b')
+            return (re.findall(pattern, content))
 
-        pass
+
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import checker
 
-def main:
-   path
-   pass
+def main():
+   path = r''
+   file = (input('Введите путь к  файлу: '))
+   path = rf'{file}'.replace('"', '')
+   checker_ex = checker.Checker(path)
+   print(checker_ex.check_file())
 
 if __name__ == '__main__':
    main()
-   pass
