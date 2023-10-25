93e8c5ac6107f75c2f36a87eeab69c2eb62a3efe
dadayasho <lmaolol228@mail.ru>
2023-10-25 23:44:27 +0300

    [patterns added]

--- checker.py
+++ checker.py
@@ -9,7 +9,10 @@ class Checker:
         with open(self.file, 'r') as file_txt:
             content = file_txt.read()
             content = re.sub(r'\n', ' ', content)
-            pattern = (r'\b[23561]\d{12}(?:\d{3}|\d{5}|\d{7}|\d{8})?\b')
-            return (re.findall(pattern, content))
+            pattern = (r'\b[23561]\d{12}(?:\d{3}|\d{5}|\d{7}|\d{8})?\b') #проверка номера карты
+            pattern2 = r'\b\d{1,12}\/\d{1,99}\b' #проверка cvv
+            pattern3 = r'\b[A-Za-z ]{3,20}\b' #проверка имени
+            return (re.findall(pattern3, content))
 
 
+#"C:\Users\bende\Desktop\Рабочий стол\работы уник\test.txt"
