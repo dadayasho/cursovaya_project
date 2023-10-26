60fe5316a3ebcb8f5c7e389684ed9490d353a1c8
Максим Порфирьев <maxim_p@MacBook-Pro-Maksim.local>
2023-10-26 12:16:38 +0300

    [checker corrected]

--- checker.py
+++ checker.py
@@ -6,13 +6,19 @@ class Checker:
         self.file = file_path
 
     def check_file(self):
+        pattern = (r'\b[23561]\d{12}(?:\d{3}|\d{5}|\d{7}|\d{8})?\b') #проверка номера карты
+        pattern2 = r'\b\d{1,12}\/\d{1,99}\b' #проверка cvv
+        pattern3 = r'\b[A-Za-z ]{3,20}\b' #проверка имени
         with open(self.file, 'r') as file_txt:
             content = file_txt.read()
             content = re.sub(r'\n', ' ', content)
-            pattern = (r'\b[23561]\d{12}(?:\d{3}|\d{5}|\d{7}|\d{8})?\b') #проверка номера карты
-            pattern2 = r'\b\d{1,12}\/\d{1,99}\b' #проверка cvv
-            pattern3 = r'\b[A-Za-z ]{3,20}\b' #проверка имени
-            return (re.findall(pattern3, content))
+            if re.findall(pattern, content):
+                if re.findall(pattern2, content) or re.findall(pattern3, content) or (re.findall(pattern2, content) and re.findall(pattern3, content)):
+                    return True
+                return True
+            else:
+                return False
+
 
 
 #"C:\Users\bende\Desktop\Рабочий стол\работы уник\test.txt"
