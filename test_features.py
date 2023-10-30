77d2ad1381dce5d887acfe55545f52836a700cd1
Максим Порфирьев <maxim_p@MacBook-Pro-Maksim.local>
2023-10-30 12:22:21 +0300

    [code logic rebuilded]

--- test_features.py
+++ test_features.py
@@ -1,4 +1,4 @@
-import openpyxl
+import opejjjnpyxl
 import re
 
 path = r''
@@ -12,18 +12,20 @@ sheet = workbook.active
 
 # Регулярные выражения для поиска
 pattern = re.compile(r'\b[23561]\d{12}(?:\d{3}|\d{5}|\d{7}|\d{8})?\b') #проверка номера карты
-pattern_for_cvv = re.compile(r'\b\d{1,12}\/\d{1,99}\b') #проверка cvv
+pattern_for_cvv = re.compile(r'\b\d{1,12}\/\d{1,99}\b')
+pattern_for_name = re.compile(r'\b[A-Za-z ]{3,20}\b')
+#проверка cvv
 # Пройдемся по каждой ячейке в листе
 for row in sheet.iter_rows():
     for cell in row:
         # Проверяем, содержит ли ячейка текст
-        if cell.data_type == 's':
+        if cell.data_type != 's':
+            print(f"нет")
+        else:
             if pattern.search(cell.value):
-
                 # Если найдено первое слово, ищем второе
-                if pattern_for_cvv.search(cell.value):
+                if pattern_for_cvv.search(cell.value) or pattern_for_name.search(cell.value) or pattern_for_cvv.search(cell.value) and pattern_for_name.search(cell.value):
                     print(f"да")
-                print(f"да")
             else:
                 print(f"нет")
 #добавить паттерн для поиска имени, испарвить условие, создать хэш таблицу с ключами да/нет
