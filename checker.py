d9106ceeed434a3b93b1e5c7eed56b52b0bcf7be
Максим Порфирьев <maxim_p@MacBook-Pro-Maksim.local>
2023-11-01 14:35:42 +0300

    [excel checker created, added to main]

--- checker.py
+++ checker.py
@@ -1,12 +1,12 @@
 import re
-
+import openpyxl
 
 class Checker:
     def __init__(self, file_path):
         self.file = file_path
 
     def check_file(self):
-        pattern = (r'\b[23561]\d{12}(?:\d{3}|\d{5}|\d{7}|\d{8})?\b') #проверка номера карты
+        pattern = r'\b[23561]\d{12}(?:\d{3}|\d{5}|\d{7}|\d{8})?\b' #проверка номера карты
         pattern2 = r'\b\d{1,12}\/\d{1,99}\b' #проверка cvv
         pattern3 = r'\b[A-Za-z ]{3,20}\b' #проверка имени
         with open(self.file, 'r') as file_txt:
@@ -15,10 +15,23 @@ class Checker:
             if re.findall(pattern, content):
                 if re.findall(pattern2, content) or re.findall(pattern3, content) or (re.findall(pattern2, content) and re.findall(pattern3, content)):
                     return True
-                return True
             else:
                 return False
-
-
-
+    def checker_for_excel(self):
+        workbook = openpyxl.load_workbook(self.path)
+        sheet = workbook.active
+        pattern = re.compile(r'\b[23561]\d{12}(?:\d{3}|\d{5}|\d{7}|\d{8})?\b')
+        pattern_for_cvv = re.compile(r'\b\d{1,12}\/\d{1,99}\b')
+        pattern_for_name = re.compile(r'\b[A-Za-z ]{3,20}\b')
+        for row in sheet.iter_rows(): # Пройдемся по каждой ячейке в листе
+            for cell in row:
+                if cell.data_type != 's': # Проверяем, содержит ли ячейка текст
+                    return(f"нет")
+                else:
+                    if pattern.search(cell.value):
+                        if pattern_for_cvv.search(cell.value) or pattern_for_name.search(cell.value) or pattern_for_cvv.search(cell.value) and pattern_for_name.search(cell.value):
+                            return (f"да")
+                    else:
+                        return(f"нет")
+        workbook.close()
 #"C:\Users\bende\Desktop\Рабочий стол\работы уник\test.txt"
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import checker
+from pathlib import Path
 
+def file_extesion(file_path):
+   return Path(file_path).suffix
 def main():
    path = r''
    file = (input('Введите путь к  файлу: '))
    path = rf'{file}'.replace('"', '')
    checker_ex = checker.Checker(path)
-   print(checker_ex.check_file())
+   if file_extesion(path) == '.txt':
+      print(checker_ex.check_file())
+   if file_extesion(path) in ['.xlx', '.xlxs']:
+      print(checker_ex.checker_for_excel())
 
 if __name__ == '__main__':
    main()
