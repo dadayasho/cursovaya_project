c319a9c20fdc1c0637e528d5f338de9405190032
dadayasho <lmaolol228@mail.ru>
2023-12-09 18:34:20 +0300

    [main fixed]

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-import checker # импортируем наш файл с классом
-from pathlib import Path # библиотека для работы с путями к файлам
+import checker  # импортируем наш файл с классом
+from pathlib import Path  # библиотека для работы с путями к файлам
 
-def file_extesion(file_path): # 'Вырезаем расширение в виде текста'
-   return Path(file_path).suffix
+
+def file_extesion(file_path):  # 'Вырезаем расширение в виде текста'
+    return Path(file_path).suffix
+
+
 def main():
-   path = r''
-   file = (input('Введите путь к  файлу: ')) # вводим путь к файлу
-   path = rf'{file}'.replace('"', '') # заменяем кавычки на те, с которыми не будет возникать ошибок
-   checker_ex = checker.Checker(path) # создаем экземпляр класс, в который инициализируем наш путь к файлу
-   # Далее исходя из расширения файла обращаемся к функциям
-   if file_extesion(path) == '.txt':
-      print(checker_ex.check_file())
-   if file_extesion(path) in ['.xlx', '.xlxs']:
-      print(checker_ex.checker_for_excel())
-   if file_extesion(path) in ['.csv']:
-      print(checker_ex.checker_for_csv())
-   if file_extesion(path) in ['.xml']:
-      print(checker_ex.checker_for_xml())
+    path = r''
+    file = (input('Введите путь к  файлу: '))  # вводим путь к файлу
+    path = rf'{file}'.replace('"', '')  # заменяем кавычки на те, с которыми не будет возникать ошибок
+    checker_ex = checker.Checker(path)  # создаем экземпляр класс, в который инициализируем наш путь к файлу
+    # Далее исходя из расширения файла обращаемся к функциям
+    if file_extesion(path) == '.txt':
+        print(checker_ex.check_file())
+    if file_extesion(path) in ['.xlsx']:
+        print(checker_ex.checker_for_excel())
+    if file_extesion(path) in ['.csv']:
+        print(checker_ex.checker_for_csv())
+    if file_extesion(path) in ['.xml']:
+        print(checker_ex.checker_for_xml())
+
 
 if __name__ == '__main__':
-   main()
+    main()
